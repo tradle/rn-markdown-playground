760dffc9cac1750d0f4d67cb55d2c238d97b2cbf
Mark Vayngrib <mark@tradle.io>
2017-06-09 02:29:02 -0400

    fix dist

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+#git subtree push --prefix dist origin gh-pages
+git push origin `git subtree split --prefix dist master`:gh-pages --force
